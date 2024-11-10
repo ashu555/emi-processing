bb8e074b5b59e19ffac6c847566d425cd99b1d50
Ashutosh pathak <ashutosh.pathak@ladybirdweb.com>
2024-11-10 10:39:50 +0530

    Updated seeder file

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,9 +15,34 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        \DB::table('loan_details')->insert([
+            [
+                'clientid' => 1001,
+                'num_of_payment' => 12,
+                'first_payment_date' => '2018-06-29',
+                'last_payment_date' => '2019-05-29',
+                'loan_amount' => 1550.00,
+            ],
+            [
+                'clientid' => 1003,
+                'num_of_payment' => 7,
+                'first_payment_date' => '2019-02-15',
+                'last_payment_date' => '2019-08-15',
+                'loan_amount' => 6851.94,
+            ],
+            [
+                'clientid' => 1005,
+                'num_of_payment' => 17,
+                'first_payment_date' => '2017-11-09',
+                'last_payment_date' => '2019-03-09',
+                'loan_amount' => 1800.01,
+            ]
+        ]);
+
+        \DB::table('users')->insert([
+            'name' => 'developer',
+            'email' => 'developer@example.com',
+            'password' => \Hash::make('Test@Password123#'),
         ]);
     }
 }
